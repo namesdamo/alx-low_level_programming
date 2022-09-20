66ea01769650f5867f36daeba2bcc781f2591f39
namesdamo <omilaget@gmail.com>
2022-09-20 15:46:05 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _puts - A function that prints a string 
  * followed by a new line to stdout
  * @str: An input string
  * Return: Nothing
  */
-
 void _puts(char *str)
 {
 int i = 0;
