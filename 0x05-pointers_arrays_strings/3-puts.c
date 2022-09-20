bd7b309a096ebd8a998bfd543b3ce1f800200629
namesdamo <omilaget@gmail.com>
2022-09-20 15:50:29 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts - A function that prints a string 
+ * _puts - A function that prints a string
  * followed by a new line to stdout
  * @str: An input string
  * Return: Nothing
