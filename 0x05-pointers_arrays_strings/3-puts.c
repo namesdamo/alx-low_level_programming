f774f94cd2f0f8f4c73c9a1eaac4eb001c351699
omilani damola <omilaget@gmail.com>
2022-09-19 17:01:45 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the lenght of a string
+ * @s: An integer input pointer
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+
